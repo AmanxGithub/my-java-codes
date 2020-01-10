448baaf2c414f5201d0c37ee831669633d286fa5
AmanxGithub <vicky.amangupta@gmail.com>
2020-01-10 18:58:34 +0530

    Throws Exception

--- src/ExceptionThrown.java
+++ src/ExceptionThrown.java
@@ -1,15 +1,12 @@
-class ExceptionThrown
-{
-    public static void main(String args[])
-    {
+class ExceptionThrown {
+    public static void main(String args[]) {
         try {
             int a = 30, b = 0;
-            int c = a/b;
-            System.out.println ("Result = " + c);
-        }
-        catch(ArithmeticException e) {
-            System.out.println ("Can't divide a number by 0");
+            int c = a / b;
+            System.out.println("Result = " + c);
+        } catch (ArithmeticException e) {
+            System.out.println("Can't divide a number by 0");
         }
     }
-}
 
+}
