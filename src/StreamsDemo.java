44b6be3773e29abf98251afd866dcd626b72bf22
AmanxGithub <vicky.amangupta@gmail.com>
2020-01-16 15:04:24 +0530

    streams

---
+++ src/StreamsDemo.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        List<Integer> values= Arrays.asList(1,2,3,4,5,6);
+        //values.forEach(i -> System.out.println(i));
+        //Internal Iteration
+
+        Consumer<Integer> c=new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+        values.forEach(c);
+    }
+}
