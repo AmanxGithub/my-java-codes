56549ac1dfa7016c2d446ebe2cc9d51ef706303f
AmanxGithub <vicky.amangupta@gmail.com>
2020-01-16 15:36:39 +0530

    streams paraller vs serial

---
+++ src/StreamsDemo2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamsDemo2 {
+    public static void main(String[] args) {
+        List<Integer> values =new ArrayList<>();
+        for(int i=1;i<=100;i++)
+        {
+            values.add(i);
+        }
+        values.stream().forEach(System.out::println);
+
+        values.parallelStream().forEach(System.out::println);
+
+    }
+}
