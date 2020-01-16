9fbdaee2bb8c76ea591bdbee404622845e376ace
AmanxGithub <vicky.amangupta@gmail.com>
2020-01-16 15:27:43 +0530

    lambda, call by method

--- src/StreamsDemo.java
+++ src/StreamsDemo.java
@@ -5,16 +5,16 @@ import java.util.function.Consumer;
 public class StreamsDemo {
     public static void main(String[] args) {
         List<Integer> values= Arrays.asList(1,2,3,4,5,6);
-        //values.forEach(i -> System.out.println(i));
-        //Internal Iteration
+        values.forEach(i -> System.out.println(i));
+       // Internal Iteration
 
-//        Consumer<Integer> c=new Consumer<Integer>() {
-//            @Override
-//            public void accept(Integer integer) {
-//                System.out.println(integer);
-//            }
-//        };
-//        values.forEach(c);
+        Consumer<Integer> c=new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+        values.forEach(c);
 
 
 
