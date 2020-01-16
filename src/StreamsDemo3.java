3edf6e1a36b0b4eb12ecef802602fe161e5d0acb
AmanxGithub <vicky.amangupta@gmail.com>
2020-01-16 16:03:21 +0530

    streams map,reduce

---
+++ src/StreamsDemo3.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsDemo3 {
+    public static void main(String[] args) {
+        List<Integer> values = Arrays.asList(1,2,3,4,5,6);
+        System.out.println(values.stream().map(i->i*2).reduce(0,(c,e)->c+e));
+    }
+}
